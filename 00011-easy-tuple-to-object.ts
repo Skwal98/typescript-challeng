795d17593813d8934a0d1fa6d323c10ce47a151c
a.levkovskiy <levkalex98@gmail.com>
2022-04-21 00:40:19 +0300

    easy end

---
+++ 00011-easy-tuple-to-object.ts
@@ -0,0 +1,29 @@
+﻿
+/* _____________ Your Code Here _____________ */
+
+type TupleToObject<T extends ReadonlyArray<string>> = { [P in T[number]]: P }
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+
+type cases = [
+    Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+]
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>
+
+
+
+//comments
+type test = typeof tuple[number]; //"tesla" | "model 3" | "model X" | "model Y" (extract array items)
+type result = { [P in test ] : P };
+const a: result = {
+    tesla: 'tesla',
+    'model 3': 'model 3', 
+    'model X': 'model X', 
+    'model Y': 'model Y'
+}
---
+++ 00898-easy-includes.ts
@@ -0,0 +1,30 @@
+﻿/* _____________ Your Code Here _____________ */
+
+type Includes<T extends any[], U> = T extends [infer X, ...infer Y] ? Equal<X, U> extends true ? true : Includes<Y, U> : false
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+    Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+    Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+    Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+    Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+    Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+    Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
+    Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+    Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+    Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+    Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+    Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+    Expect<Equal<Includes<[1], 1 | 2>, false>>,
+    Expect<Equal<Includes<[1 | 2], 1>, false>>,
+    Expect<Equal<Includes<[null], undefined>, false>>,
+    Expect<Equal<Includes<[undefined], null>, false>>,
+]
+
+//comments
+type test = [1, 2, 3, 5, 6, 7] extends [infer X, ...infer Y] ? Y : never; //[2, 3, 5, 6, 7]
+type G =  [1, 2, 3, 5, 6, 7][number] ; //6 | 1 | 2 | 3 | 5 | 7
+type B = 1 extends G ? true : false;  //true
+//false extends boolean - true
